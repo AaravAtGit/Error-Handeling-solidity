2aa79da2fa1762de873f1fc58ed93cf4968f0da5
Aarav Shishodia <aarav54897@gmail.com>
2023-09-24 00:45:45 +0000

    first commit

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,5 +4,19 @@
 pragma solidity ^0.8.17;
 
 contract HelloWorld {
-    string public greet = "Hello World!";
+    function RequireFunc(uint i) public pure {
+        require(i > 7, "Input must be greater than 7");
+    }
+
+    function RevertFunc(uint i) public pure {
+        if (i <= 7) {
+            revert("Input must be greater than 7");
+        }
+    }
+
+    function AssertFunc(uint num) public pure {
+        assert(num == 15);
+    }
 }
+
+
